2564fa0f9b9c7f3c230dfd47aace01163ec7c546
wychwood <wychwd18@gmail.com>
2019-07-15 23:42:43 +0300

    AKS create cluster and deploy shell

---
+++ iaas/aks/create-aks.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+rg='aksdemos'
+cluster='akscluster'
+
+az group create -n $rg -l westeurope
+
+az aks create -g $rg \
+ -n $cluster \
+ --node-count 1 \
+ --generate-ssh-keys \
+ --enable-addons monitoring
+
+az aks get-credentials \
+ -g $rg \
+ -n $cluster
+
+kubectl get nodes
+
+kubectl apply -f azure-vote.yaml
+
+kubectl get service azure-vote-front --watch
